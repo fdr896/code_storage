97992a310bc754970829a3ed347b3fa13b999702
Fedor Osetrov <fiosetrova@gmail.com>
2020-04-20 18:12:14 -0400

    Docker bug fixes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# running images simultaniously.
-docker run -p 8080:8080 code_storage/rest-api & docker run -p 5000:5000 code_storage/svelte
+# start containers simultaniously.
+docker start rest-api & docker start svelte
